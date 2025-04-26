bbd6b7c1146161b13c0433dc7787447c07475332
monique1012 <mueller.monique1@web.de>
2025-04-26 12:22:57 +0200

    index.ts fertig

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 // CSS IMPORT IN TS NUR ÜBER VITE MÖGLICH
 import './styles/styles.css';
 
+const input = document.getElementById('eingabefeld') as HTMLInputElement;
+const button = document.getElementById('weiter') as HTMLButtonElement;
+
+button.addEventListener('click', () => {
+    console.log("Button wurde geklickt!");
+
+    if (input.value === '') {
+        alert(`Bitte gib einen Namen ein!`);
+        return;
+    } else {
+        console.log(input.value);
+        localStorage.setItem("Username", input.value);
+        window.location.href = "tisch.html";
+    }
+});
